4b4af1859fdc0889630cbe247c93b194b6c6cd38
eritaakash <eritaakash@gmail.com>
2024-03-31 13:15:19 +0530

    dotenv issue resolved

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
-require('dotenv').config();
 
 const nextConfig = {
     reactStrictMode: true,
-    env: {
-        api_key: process.env.API_KEY
-    }
 }
 
 module.exports = nextConfig
