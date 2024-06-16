d2dceb6005d8d891c40ff2fd0a977719b707efca
eritaakash <eritaakash@gmail.com>
2024-06-16 18:01:30 +0530

    pfp

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -15,7 +15,7 @@ const Intro = () => {
 
             <div className={styles.imageContainer}>
                 <Image
-                    src="/assets/author.png"
+                    src="/images/author.png"
                     alt="Aakash"
                     width={200}
                     height={200}
